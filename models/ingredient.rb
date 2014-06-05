8dc41abfa146af63eb3c4c5680c19800c71a078c
Maimer <nesnl23@yahoo.com>
2014-06-05 13:14:36 -0400

    Added individual recipe pages

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient
+  attr_reader :name
+  def initialize(ingredient)
+    @name = ingredient
+  end
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,10 +1,14 @@
 require 'pg'
+require 'pry'
 
 class Recipe
-  attr_reader :id, :name
-  def initialize(id, name)
+  attr_reader :id, :name, :instructions, :description, :ingredients
+  def initialize(id, name, instructions = nil, description = nil, ingredients = [])
     @id = id
     @name = name
+    @instructions = instructions
+    @description = description
+    @ingredients = ingredients
   end
 
   def self.db_connection
@@ -30,5 +34,22 @@ class Recipe
       all_recipes << Recipe.new(recipe["id"], recipe["name"])
     end
     all_recipes
+  end
+
+  def self.find(id)
+    recipe = []
+    db_connection do |conn|
+      query = "SELECT recipes.name AS name, recipes.id, recipes.instructions, recipes.description,
+               ingredients.name AS ingredient
+               FROM recipes
+                JOIN ingredients ON recipes.id = ingredients.recipe_id
+               WHERE recipes.id = #{id}"
+      recipe = conn.exec(query)
+    end
+    array = []
+    recipe.each do |row|
+      array << Ingredient.new(row["ingredient"])
+    end
+    Recipe.new(recipe[0]["id"], recipe[0]["name"], recipe[0]["instructions"], recipe[0]["description"], array)
   end
 end
