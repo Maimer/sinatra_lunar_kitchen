fce5fc30011729b2f4fd4e63fc8f5010e102cb1b
Maimer <nesnl23@yahoo.com>
2014-06-05 11:59:56 -0400

    Added methods to recipe class to return all recipes and ids as recipe objects

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,34 @@
+require 'pg'
+
 class Recipe
+  attr_reader :id, :name
+  def initialize(id, name)
+    @id = id
+    @name = name
+  end
+
+  def self.db_connection
+    begin
+      connection = PG.connect(dbname: 'recipes')
+      yield(connection)
+    ensure
+      connection.close
+    end
+  end
+
+  def self.all
+    results = []
+    db_connection do |conn|
+      query = "SELECT recipes.name, recipes.id, recipes.description, recipes.instructions
+               FROM recipes
+               WHERE recipes.description IS NOT NULL AND recipes.instructions IS NOT NULL
+               ORDER BY recipes.name"
+      results = conn.exec(query)
+    end
+    all_recipes = []
+    results.each do |recipe|
+      all_recipes << Recipe.new(recipe["id"], recipe["name"])
+    end
+    all_recipes
+  end
 end
